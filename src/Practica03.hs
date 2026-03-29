e1db02833f54a1569bf283a434740da5de35bf31
Aidee <aidee.santana.24@ciencias.unam.mx>
2026-03-29 09:28:29 -0600

    src segundo cambio

--- src/Practica03.hs
+++ src/Practica03.hs
@@ -32,12 +32,28 @@ FORMAS NORMALES
 
 --Ejercicio 1
 fnn :: Prop -> Prop
-fnn = undefined
+fnn p = p
+fnn (Not (Not p)) = fnn p
+fnn (And p q) = And (fnn p) (fnn q)
+fnn (Or p q) = Or (fnn p) (fnn q)
+fnn (Impl p q) = fnn (Or (Not p) q)
+fnn (Syss p q) = fnn (And (Impl p q) (Impl q p))
+fnn (Not (And p q)) = Or (fnn (Not p)) (fnn (Not q))
+fnn (Not (Or p q)) = And (fnn (Not p)) (fnn (Not q))
 
 
 --Ejercicio 2
 fnc :: Prop -> Prop
-fnc = undefined
+fnc p = dist (fnn p)
+
+-- Función auxiliar para usar la propiedad de distributividad.
+dist :: Prop -> Prop
+dist p = p 
+dist (And p q) = And (dist p) (dist q)
+dist (Or p q) = case (dist p, dist q) of
+    (p', And q' r') -> And (dist (Or p' q')) (dist (Or p' r'))
+    (And p' q', r') -> And (dist (Or p' r')) (dist (Or q' r'))
+    (p', q') -> Or p' q'
 
 {-
 RESOLUCION BINARIA
